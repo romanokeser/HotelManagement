7043f03dd57858bc6082801123326c3261266f09
RomanoK <romanokeser@gmail.com>
2022-09-21 21:40:42 +0200

    Fixed bug when importing new client, finished: adding, deleting and editing client from the database

--- HotelApp/ClientInfo.Designer.cs
+++ HotelApp/ClientInfo.Designer.cs
@@ -28,14 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle13 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle14 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle15 = new System.Windows.Forms.DataGridViewCellStyle();
             this.panel1 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
+            this.clientCtry = new System.Windows.Forms.ComboBox();
+            this.addBtn = new System.Windows.Forms.Button();
+            this.editBtn = new System.Windows.Forms.Button();
+            this.deleteBtn = new System.Windows.Forms.Button();
+            this.clientNameSearchBox = new Guna.UI2.WinForms.Guna2TextBox();
+            this.searchBtn = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.dateLabel = new System.Windows.Forms.Label();
+            this.clientIdTextbox = new System.Windows.Forms.TextBox();
+            this.clientNameTextbox = new System.Windows.Forms.TextBox();
+            this.clientPhoneTextbox = new System.Windows.Forms.TextBox();
+            this.ClientGridView = new Guna.UI2.WinForms.Guna2DataGridView();
             this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ClientGridView)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.dateLabel);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
@@ -53,19 +71,196 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Client information";
             // 
+            // clientCtry
+            // 
+            this.clientCtry.FormattingEnabled = true;
+            this.clientCtry.Items.AddRange(new object[] {
+            "CROATIA",
+            "BOSNIA",
+            "SERBIA",
+            "AUSTRIA",
+            "LOREM",
+            "IPSUM"});
+            this.clientCtry.Location = new System.Drawing.Point(49, 369);
+            this.clientCtry.Name = "clientCtry";
+            this.clientCtry.Size = new System.Drawing.Size(161, 21);
+            this.clientCtry.TabIndex = 4;
+            this.clientCtry.Text = "Country";
+            // 
+            // addBtn
+            // 
+            this.addBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addBtn.Location = new System.Drawing.Point(57, 484);
+            this.addBtn.Name = "addBtn";
+            this.addBtn.Size = new System.Drawing.Size(89, 52);
+            this.addBtn.TabIndex = 6;
+            this.addBtn.Text = "ADD";
+            this.addBtn.UseVisualStyleBackColor = true;
+            this.addBtn.Click += new System.EventHandler(this.addBtn_Click);
+            // 
+            // editBtn
+            // 
+            this.editBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.editBtn.Location = new System.Drawing.Point(170, 484);
+            this.editBtn.Name = "editBtn";
+            this.editBtn.Size = new System.Drawing.Size(89, 52);
+            this.editBtn.TabIndex = 7;
+            this.editBtn.Text = "EDIT";
+            this.editBtn.UseVisualStyleBackColor = true;
+            this.editBtn.Click += new System.EventHandler(this.editBtn_Click);
+            // 
+            // deleteBtn
+            // 
+            this.deleteBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.deleteBtn.Location = new System.Drawing.Point(105, 574);
+            this.deleteBtn.Name = "deleteBtn";
+            this.deleteBtn.Size = new System.Drawing.Size(89, 52);
+            this.deleteBtn.TabIndex = 8;
+            this.deleteBtn.Text = "delete";
+            this.deleteBtn.UseVisualStyleBackColor = true;
+            this.deleteBtn.Click += new System.EventHandler(this.deleteBtn_Click);
+            // 
+            // clientNameSearchBox
+            // 
+            this.clientNameSearchBox.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.clientNameSearchBox.DefaultText = "Client name";
+            this.clientNameSearchBox.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.clientNameSearchBox.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.clientNameSearchBox.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.clientNameSearchBox.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.clientNameSearchBox.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.clientNameSearchBox.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.clientNameSearchBox.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.clientNameSearchBox.Location = new System.Drawing.Point(295, 171);
+            this.clientNameSearchBox.Name = "clientNameSearchBox";
+            this.clientNameSearchBox.PasswordChar = '\0';
+            this.clientNameSearchBox.PlaceholderText = "";
+            this.clientNameSearchBox.SelectedText = "";
+            this.clientNameSearchBox.Size = new System.Drawing.Size(161, 30);
+            this.clientNameSearchBox.TabIndex = 9;
+            // 
+            // searchBtn
+            // 
+            this.searchBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.searchBtn.Location = new System.Drawing.Point(480, 171);
+            this.searchBtn.Name = "searchBtn";
+            this.searchBtn.Size = new System.Drawing.Size(89, 30);
+            this.searchBtn.TabIndex = 10;
+            this.searchBtn.Text = "Search";
+            this.searchBtn.UseVisualStyleBackColor = true;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // dateLabel
+            // 
+            this.dateLabel.AutoSize = true;
+            this.dateLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateLabel.Location = new System.Drawing.Point(494, 126);
+            this.dateLabel.Name = "dateLabel";
+            this.dateLabel.Size = new System.Drawing.Size(48, 20);
+            this.dateLabel.TabIndex = 1;
+            this.dateLabel.Text = "Date";
+            // 
+            // clientIdTextbox
+            // 
+            this.clientIdTextbox.Location = new System.Drawing.Point(49, 203);
+            this.clientIdTextbox.Name = "clientIdTextbox";
+            this.clientIdTextbox.Size = new System.Drawing.Size(161, 20);
+            this.clientIdTextbox.TabIndex = 11;
+            // 
+            // clientNameTextbox
+            // 
+            this.clientNameTextbox.Location = new System.Drawing.Point(49, 243);
+            this.clientNameTextbox.Name = "clientNameTextbox";
+            this.clientNameTextbox.Size = new System.Drawing.Size(161, 20);
+            this.clientNameTextbox.TabIndex = 12;
+            // 
+            // clientPhoneTextbox
+            // 
+            this.clientPhoneTextbox.Location = new System.Drawing.Point(49, 283);
+            this.clientPhoneTextbox.Name = "clientPhoneTextbox";
+            this.clientPhoneTextbox.Size = new System.Drawing.Size(161, 20);
+            this.clientPhoneTextbox.TabIndex = 13;
+            // 
+            // ClientGridView
+            // 
+            dataGridViewCellStyle13.BackColor = System.Drawing.Color.White;
+            this.ClientGridView.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle13;
+            dataGridViewCellStyle14.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle14.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle14.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle14.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle14.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle14.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle14.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.ClientGridView.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle14;
+            this.ClientGridView.ColumnHeadersHeight = 24;
+            this.ClientGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.EnableResizing;
+            dataGridViewCellStyle15.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle15.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle15.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle15.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            dataGridViewCellStyle15.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle15.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            dataGridViewCellStyle15.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.ClientGridView.DefaultCellStyle = dataGridViewCellStyle15;
+            this.ClientGridView.GridColor = System.Drawing.Color.Black;
+            this.ClientGridView.Location = new System.Drawing.Point(295, 226);
+            this.ClientGridView.Name = "ClientGridView";
+            this.ClientGridView.RowHeadersVisible = false;
+            this.ClientGridView.Size = new System.Drawing.Size(478, 350);
+            this.ClientGridView.TabIndex = 14;
+            this.ClientGridView.ThemeStyle.AlternatingRowsStyle.BackColor = System.Drawing.Color.White;
+            this.ClientGridView.ThemeStyle.AlternatingRowsStyle.Font = null;
+            this.ClientGridView.ThemeStyle.AlternatingRowsStyle.ForeColor = System.Drawing.Color.Empty;
+            this.ClientGridView.ThemeStyle.AlternatingRowsStyle.SelectionBackColor = System.Drawing.Color.Empty;
+            this.ClientGridView.ThemeStyle.AlternatingRowsStyle.SelectionForeColor = System.Drawing.Color.Empty;
+            this.ClientGridView.ThemeStyle.BackColor = System.Drawing.Color.White;
+            this.ClientGridView.ThemeStyle.GridColor = System.Drawing.Color.Black;
+            this.ClientGridView.ThemeStyle.HeaderStyle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            this.ClientGridView.ThemeStyle.HeaderStyle.BorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            this.ClientGridView.ThemeStyle.HeaderStyle.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ClientGridView.ThemeStyle.HeaderStyle.ForeColor = System.Drawing.Color.White;
+            this.ClientGridView.ThemeStyle.HeaderStyle.HeaightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.EnableResizing;
+            this.ClientGridView.ThemeStyle.HeaderStyle.Height = 24;
+            this.ClientGridView.ThemeStyle.ReadOnly = false;
+            this.ClientGridView.ThemeStyle.RowsStyle.BackColor = System.Drawing.Color.White;
+            this.ClientGridView.ThemeStyle.RowsStyle.BorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SingleHorizontal;
+            this.ClientGridView.ThemeStyle.RowsStyle.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ClientGridView.ThemeStyle.RowsStyle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            this.ClientGridView.ThemeStyle.RowsStyle.Height = 22;
+            this.ClientGridView.ThemeStyle.RowsStyle.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            this.ClientGridView.ThemeStyle.RowsStyle.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            this.ClientGridView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.ClientGridView_CellContentClick);
+            // 
             // ClientInfo
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 665);
+            this.Controls.Add(this.ClientGridView);
+            this.Controls.Add(this.clientPhoneTextbox);
+            this.Controls.Add(this.clientNameTextbox);
+            this.Controls.Add(this.clientIdTextbox);
+            this.Controls.Add(this.searchBtn);
+            this.Controls.Add(this.clientNameSearchBox);
+            this.Controls.Add(this.deleteBtn);
+            this.Controls.Add(this.editBtn);
+            this.Controls.Add(this.addBtn);
+            this.Controls.Add(this.clientCtry);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "ClientInfo";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "ClientInfo";
+            this.Load += new System.EventHandler(this.ClientInfo_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ClientGridView)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -73,5 +268,17 @@
 
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox clientCtry;
+        private System.Windows.Forms.Button addBtn;
+        private System.Windows.Forms.Button editBtn;
+        private System.Windows.Forms.Button deleteBtn;
+        private Guna.UI2.WinForms.Guna2TextBox clientNameSearchBox;
+        private System.Windows.Forms.Button searchBtn;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label dateLabel;
+        private System.Windows.Forms.TextBox clientIdTextbox;
+        private System.Windows.Forms.TextBox clientNameTextbox;
+        private System.Windows.Forms.TextBox clientPhoneTextbox;
+        private Guna.UI2.WinForms.Guna2DataGridView ClientGridView;
     }
 }
--- HotelApp/ClientInfo.cs
+++ HotelApp/ClientInfo.cs
@@ -7,14 +7,91 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace HotelApp
 {
     public partial class ClientInfo : Form
     {
+        SqlConnection conn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Romano\Documents\Hoteldb.mdf;Integrated Security=True;Connect Timeout=30");
+
+        public void Populate()
+        {
+            conn.Open();
+            string query = "SELECT * FROM Client_tbl";
+            SqlDataAdapter da = new SqlDataAdapter(query, conn);
+            SqlCommandBuilder cbuilder = new SqlCommandBuilder(da);
+            var ds = new DataSet();
+            da.Fill(ds);
+            ClientGridView.DataSource = ds.Tables[0];
+
+            conn.Close();
+        }
+
         public ClientInfo()
         {
             InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            dateLabel.Text = DateTime.Now.ToLongDateString();
+        }
+
+        private void ClientInfo_Load(object sender, EventArgs e)
+        {
+            dateLabel.Text = DateTime.Now.ToLongDateString();
+            Populate();
+        }
+
+        private void addBtn_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+
+            SqlCommand cmd = new SqlCommand("INSERT INTO Client_tbl VALUES('"
+                + clientIdTextbox.Text + "','"
+                + clientNameTextbox.Text + "','"
+                + clientPhoneTextbox.Text + "','"
+                + clientCtry.SelectedItem.ToString() + "')", conn);
+
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("Client added succesfully");
+            conn.Close();
+            Populate();
+        }
+
+        private void ClientGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            clientIdTextbox.Text = ClientGridView.SelectedRows[0].Cells[0].Value.ToString();
+            clientNameTextbox.Text = ClientGridView.SelectedRows[0].Cells[1].Value.ToString();
+            clientPhoneTextbox.Text = ClientGridView.SelectedRows[0].Cells[2].Value.ToString();
+        }
+
+        private void editBtn_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+
+            string query = "UPDATE Client_tbl SET ClientName = '" + clientNameTextbox.Text
+                + "', ClientPhone = '" + clientPhoneTextbox.Text
+                + "', ClientCountry= '" + clientCtry.SelectedItem.ToString()
+                + "' WHERE ClientId = " + clientIdTextbox.Text + ";";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("Client succesfully edited");
+            conn.Close();
+            Populate();
+        }
+
+        private void deleteBtn_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+
+            string query = "DELETE FROM Client_tbl WHERE ClientId = " + clientIdTextbox.Text + "";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("Client succesfully deleted");
+            conn.Close();
+            Populate();
         }
     }
 }
