01b6f0e8a553ed56e51328f33804fc0e5df15dc8
RomanoK <romanokeser@gmail.com>
2022-09-22 20:04:03 +0200

    Implemented Reservation module

--- HotelApp/ClientInfo.Designer.cs
+++ HotelApp/ClientInfo.Designer.cs
@@ -29,9 +29,9 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ClientInfo));
             this.panel1 = new System.Windows.Forms.Panel();
             this.dateLabel = new System.Windows.Forms.Label();
@@ -48,6 +48,7 @@
             this.clientPhoneTextbox = new System.Windows.Forms.TextBox();
             this.ClientGridView = new Guna.UI2.WinForms.Guna2DataGridView();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.RoomsDropbox = new System.Windows.Forms.ComboBox();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.ClientGridView)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
@@ -144,7 +145,7 @@
             this.clientSearch.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
             this.clientSearch.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.clientSearch.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
-            this.clientSearch.Location = new System.Drawing.Point(295, 171);
+            this.clientSearch.Location = new System.Drawing.Point(458, 171);
             this.clientSearch.Name = "clientSearch";
             this.clientSearch.PasswordChar = '\0';
             this.clientSearch.PlaceholderText = "";
@@ -155,7 +156,7 @@
             // searchBtn
             // 
             this.searchBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.searchBtn.Location = new System.Drawing.Point(480, 171);
+            this.searchBtn.Location = new System.Drawing.Point(643, 171);
             this.searchBtn.Name = "searchBtn";
             this.searchBtn.Size = new System.Drawing.Size(89, 30);
             this.searchBtn.TabIndex = 10;
@@ -190,26 +191,26 @@
             // 
             // ClientGridView
             // 
-            dataGridViewCellStyle4.BackColor = System.Drawing.Color.White;
-            this.ClientGridView.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle4;
-            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
-            dataGridViewCellStyle5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle5.ForeColor = System.Drawing.Color.White;
-            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.ClientGridView.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle5;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.White;
+            this.ClientGridView.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.ClientGridView.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
             this.ClientGridView.ColumnHeadersHeight = 24;
             this.ClientGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.EnableResizing;
-            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle6.BackColor = System.Drawing.Color.White;
-            dataGridViewCellStyle6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
-            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
-            dataGridViewCellStyle6.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
-            dataGridViewCellStyle6.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.ClientGridView.DefaultCellStyle = dataGridViewCellStyle6;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.ClientGridView.DefaultCellStyle = dataGridViewCellStyle3;
             this.ClientGridView.GridColor = System.Drawing.Color.Black;
             this.ClientGridView.Location = new System.Drawing.Point(295, 226);
             this.ClientGridView.Name = "ClientGridView";
@@ -242,7 +243,7 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(587, 171);
+            this.pictureBox1.Location = new System.Drawing.Point(750, 171);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(24, 30);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -250,11 +251,21 @@
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
+            // RoomsDropbox
+            // 
+            this.RoomsDropbox.FormattingEnabled = true;
+            this.RoomsDropbox.Location = new System.Drawing.Point(342, 180);
+            this.RoomsDropbox.Name = "RoomsDropbox";
+            this.RoomsDropbox.Size = new System.Drawing.Size(91, 21);
+            this.RoomsDropbox.TabIndex = 32;
+            this.RoomsDropbox.Text = "Rooms";
+            // 
             // ClientInfo
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 665);
+            this.Controls.Add(this.RoomsDropbox);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.ClientGridView);
             this.Controls.Add(this.clientPhoneTextbox);
@@ -298,5 +309,6 @@
         private System.Windows.Forms.TextBox clientPhoneTextbox;
         private Guna.UI2.WinForms.Guna2DataGridView ClientGridView;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox RoomsDropbox;
     }
 }
--- HotelApp/ReservationInfo.Designer.cs
+++ HotelApp/ReservationInfo.Designer.cs
@@ -28,14 +28,30 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ReservationInfo));
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
             this.panel1 = new System.Windows.Forms.Panel();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.clientNameTextbox = new System.Windows.Forms.TextBox();
             this.reservationId = new System.Windows.Forms.TextBox();
             this.roomNumber = new System.Windows.Forms.TextBox();
-            this.dateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.dateIn = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dateOut = new System.Windows.Forms.DateTimePicker();
+            this.refreshImageBtn = new System.Windows.Forms.PictureBox();
+            this.roomSearchBtn = new System.Windows.Forms.Button();
+            this.reservationSearchbox = new Guna.UI2.WinForms.Guna2TextBox();
+            this.RoomGridView = new Guna.UI2.WinForms.Guna2DataGridView();
+            this.deleteRoomBtn = new System.Windows.Forms.Button();
+            this.editRoomBtn = new System.Windows.Forms.Button();
+            this.addRoomBtn = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.refreshImageBtn)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RoomGridView)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
@@ -90,19 +106,175 @@
             this.roomNumber.Size = new System.Drawing.Size(167, 20);
             this.roomNumber.TabIndex = 22;
             // 
-            // dateTimePicker
+            // dateIn
             // 
-            this.dateTimePicker.Location = new System.Drawing.Point(24, 445);
-            this.dateTimePicker.Name = "dateTimePicker";
-            this.dateTimePicker.Size = new System.Drawing.Size(200, 20);
-            this.dateTimePicker.TabIndex = 23;
+            this.dateIn.Location = new System.Drawing.Point(109, 445);
+            this.dateIn.Name = "dateIn";
+            this.dateIn.Size = new System.Drawing.Size(200, 20);
+            this.dateIn.TabIndex = 23;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(21, 446);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 20);
+            this.label1.TabIndex = 24;
+            this.label1.Text = "Date In";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(21, 502);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 20);
+            this.label2.TabIndex = 26;
+            this.label2.Text = "Date Out";
+            // 
+            // dateOut
+            // 
+            this.dateOut.Location = new System.Drawing.Point(109, 502);
+            this.dateOut.Name = "dateOut";
+            this.dateOut.Size = new System.Drawing.Size(200, 20);
+            this.dateOut.TabIndex = 25;
+            // 
+            // refreshImageBtn
+            // 
+            this.refreshImageBtn.Image = ((System.Drawing.Image)(resources.GetObject("refreshImageBtn.Image")));
+            this.refreshImageBtn.Location = new System.Drawing.Point(663, 182);
+            this.refreshImageBtn.Name = "refreshImageBtn";
+            this.refreshImageBtn.Size = new System.Drawing.Size(24, 30);
+            this.refreshImageBtn.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.refreshImageBtn.TabIndex = 30;
+            this.refreshImageBtn.TabStop = false;
+            // 
+            // roomSearchBtn
+            // 
+            this.roomSearchBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.roomSearchBtn.Location = new System.Drawing.Point(554, 182);
+            this.roomSearchBtn.Name = "roomSearchBtn";
+            this.roomSearchBtn.Size = new System.Drawing.Size(89, 30);
+            this.roomSearchBtn.TabIndex = 29;
+            this.roomSearchBtn.Text = "Search";
+            this.roomSearchBtn.UseVisualStyleBackColor = true;
+            // 
+            // reservationSearchbox
+            // 
+            this.reservationSearchbox.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.reservationSearchbox.DefaultText = "RoomSearch";
+            this.reservationSearchbox.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.reservationSearchbox.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.reservationSearchbox.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.reservationSearchbox.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.reservationSearchbox.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.reservationSearchbox.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.reservationSearchbox.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.reservationSearchbox.Location = new System.Drawing.Point(370, 182);
+            this.reservationSearchbox.Name = "reservationSearchbox";
+            this.reservationSearchbox.PasswordChar = '\0';
+            this.reservationSearchbox.PlaceholderText = "";
+            this.reservationSearchbox.SelectedText = "";
+            this.reservationSearchbox.Size = new System.Drawing.Size(161, 30);
+            this.reservationSearchbox.TabIndex = 28;
+            // 
+            // RoomGridView
+            // 
+            dataGridViewCellStyle4.BackColor = System.Drawing.Color.White;
+            this.RoomGridView.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle4;
+            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle5.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.RoomGridView.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle5;
+            this.RoomGridView.ColumnHeadersHeight = 24;
+            this.RoomGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.EnableResizing;
+            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle6.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle6.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            dataGridViewCellStyle6.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.RoomGridView.DefaultCellStyle = dataGridViewCellStyle6;
+            this.RoomGridView.GridColor = System.Drawing.Color.Black;
+            this.RoomGridView.Location = new System.Drawing.Point(322, 228);
+            this.RoomGridView.Name = "RoomGridView";
+            this.RoomGridView.RowHeadersVisible = false;
+            this.RoomGridView.Size = new System.Drawing.Size(478, 350);
+            this.RoomGridView.TabIndex = 27;
+            this.RoomGridView.ThemeStyle.AlternatingRowsStyle.BackColor = System.Drawing.Color.White;
+            this.RoomGridView.ThemeStyle.AlternatingRowsStyle.Font = null;
+            this.RoomGridView.ThemeStyle.AlternatingRowsStyle.ForeColor = System.Drawing.Color.Empty;
+            this.RoomGridView.ThemeStyle.AlternatingRowsStyle.SelectionBackColor = System.Drawing.Color.Empty;
+            this.RoomGridView.ThemeStyle.AlternatingRowsStyle.SelectionForeColor = System.Drawing.Color.Empty;
+            this.RoomGridView.ThemeStyle.BackColor = System.Drawing.Color.White;
+            this.RoomGridView.ThemeStyle.GridColor = System.Drawing.Color.Black;
+            this.RoomGridView.ThemeStyle.HeaderStyle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            this.RoomGridView.ThemeStyle.HeaderStyle.BorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            this.RoomGridView.ThemeStyle.HeaderStyle.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RoomGridView.ThemeStyle.HeaderStyle.ForeColor = System.Drawing.Color.White;
+            this.RoomGridView.ThemeStyle.HeaderStyle.HeaightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.EnableResizing;
+            this.RoomGridView.ThemeStyle.HeaderStyle.Height = 24;
+            this.RoomGridView.ThemeStyle.ReadOnly = false;
+            this.RoomGridView.ThemeStyle.RowsStyle.BackColor = System.Drawing.Color.White;
+            this.RoomGridView.ThemeStyle.RowsStyle.BorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SingleHorizontal;
+            this.RoomGridView.ThemeStyle.RowsStyle.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RoomGridView.ThemeStyle.RowsStyle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            this.RoomGridView.ThemeStyle.RowsStyle.Height = 22;
+            this.RoomGridView.ThemeStyle.RowsStyle.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            this.RoomGridView.ThemeStyle.RowsStyle.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            // 
+            // deleteRoomBtn
+            // 
+            this.deleteRoomBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.deleteRoomBtn.Location = new System.Drawing.Point(109, 604);
+            this.deleteRoomBtn.Name = "deleteRoomBtn";
+            this.deleteRoomBtn.Size = new System.Drawing.Size(89, 52);
+            this.deleteRoomBtn.TabIndex = 34;
+            this.deleteRoomBtn.Text = "delete";
+            this.deleteRoomBtn.UseVisualStyleBackColor = true;
+            // 
+            // editRoomBtn
+            // 
+            this.editRoomBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.editRoomBtn.Location = new System.Drawing.Point(181, 546);
+            this.editRoomBtn.Name = "editRoomBtn";
+            this.editRoomBtn.Size = new System.Drawing.Size(89, 52);
+            this.editRoomBtn.TabIndex = 33;
+            this.editRoomBtn.Text = "EDIT";
+            this.editRoomBtn.UseVisualStyleBackColor = true;
+            // 
+            // addRoomBtn
+            // 
+            this.addRoomBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addRoomBtn.Location = new System.Drawing.Point(25, 546);
+            this.addRoomBtn.Name = "addRoomBtn";
+            this.addRoomBtn.Size = new System.Drawing.Size(89, 52);
+            this.addRoomBtn.TabIndex = 32;
+            this.addRoomBtn.Text = "ADD";
+            this.addRoomBtn.UseVisualStyleBackColor = true;
             // 
             // ReservationInfo
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 665);
-            this.Controls.Add(this.dateTimePicker);
+            this.Controls.Add(this.deleteRoomBtn);
+            this.Controls.Add(this.editRoomBtn);
+            this.Controls.Add(this.addRoomBtn);
+            this.Controls.Add(this.refreshImageBtn);
+            this.Controls.Add(this.roomSearchBtn);
+            this.Controls.Add(this.reservationSearchbox);
+            this.Controls.Add(this.RoomGridView);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dateOut);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateIn);
             this.Controls.Add(this.roomNumber);
             this.Controls.Add(this.clientNameTextbox);
             this.Controls.Add(this.reservationId);
@@ -113,6 +285,8 @@
             this.Text = "ReservationInfo";
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.refreshImageBtn)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RoomGridView)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -126,6 +300,16 @@
         private System.Windows.Forms.TextBox clientNameTextbox;
         private System.Windows.Forms.TextBox reservationId;
         private System.Windows.Forms.TextBox roomNumber;
-        private System.Windows.Forms.DateTimePicker dateTimePicker;
+        private System.Windows.Forms.DateTimePicker dateIn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dateOut;
+        private System.Windows.Forms.PictureBox refreshImageBtn;
+        private System.Windows.Forms.Button roomSearchBtn;
+        private Guna.UI2.WinForms.Guna2TextBox reservationSearchbox;
+        private Guna.UI2.WinForms.Guna2DataGridView RoomGridView;
+        private System.Windows.Forms.Button deleteRoomBtn;
+        private System.Windows.Forms.Button editRoomBtn;
+        private System.Windows.Forms.Button addRoomBtn;
     }
 }
